095e6974b6b7be231eaed54f38e7775538fab65d
VMZHK <97471385+VMZHK@users.noreply.github.com>
2022-01-20 18:54:45 +0200

    Add files via upload

---
+++ Страница оплаты_files/msg.ru.js
@@ -0,0 +1,52 @@
+// ru
+App.msgLoc = {
+    "cardIsRequired"          : "Введите номер карты",
+    "cardNotValid"            : "Неверная карта",
+    "cardLuhnFailed"          : "Неверная карта",
+    "cardOwnerNameIsRequired" : "Введите ФИО владельца карты",
+    "cardOwnerNameJustABC"    : "Поле должно содержать только буквы",
+    "cardOwnerNameJustLat"    : "Используйте только латинские буквы",
+    "cardOwnerNameTwoWords"   : "Введите Имя и Фамилию",
+    "cardOwnerNameTooShort"   : "Имя или фамилия слишком короткие",
+    "cardDateNotValid"        : "Неверный срок",
+    "cardVisaMcOnly"          : "Введите номер карты Visa/MasterCard",
+    "useAnotherCard"          : "Используйте другую карту",
+
+    "cvvIsRequired"           : "Введите CVV2",
+    "cvvNotValid"             : "Неверный CVV2",
+
+    "emailIsRequired"         : "Введите email",
+    "emailNotValid"           : "Неверный email",
+
+    "phoneIsRequired"         : "Введите номер телефона",
+    "phoneNotValid"           : "Неверный номер телефона",
+    "phoneTooShort"           : "Номер телефона слишком короткий",
+
+    "fieldIsEmpty"            : "Заполните поле",
+
+    "cardPayerNameIsRequired" : "Введите ФИО плательщика",
+
+    "amountIsRequired"        : "Укажите сумму",
+    "amountAtLeastThan"       : "Сумма должна быть не менее ",
+    "amountShouldNotExceed"   : "Сумма не должна превышать",
+
+    "quantityIsRequired"        : "Укажите количество",
+    "quantityAtLeastThan"       : "Количество должно быть не менее",
+    "quantityShouldNotExceed"   : "Количество не должно превышать",
+
+    "wrongCode"               : "Неправильный код",
+    "inProcessing"            : "Обработка...",
+
+    "repeatModeIsRequired"    : "Укажите способ повторения",
+    "checkPaymentStatus"      : "Проверка статуса оплаты...",
+    "creditTypeRequired"      : "Выберите тип рассрочки",
+
+    "enterNameInCyrillic"     : "Введите ФИО в кириллице",
+    "doYouWantDeleteCard"     : "Вы хотите удалить эту карту?",
+
+    "passIsRequired"          : "Укажите пароль",
+    "codeIsRequired"          : "Укажите код",
+    "fieldIsRequired"         : "Заполните поле",
+    "buttonNext"              : "Далее",
+    "buttonCheckout"          : "Оплатить"
+};
